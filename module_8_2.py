c49c0af478bfb894d48bd4dd29ecf4610132f897
danil_rinatovich <danil_rinatovich@list.ru>
2024-11-30 19:47:26 +0500

    finished commit

--- module_8_2.py
+++ module_8_2.py
@@ -5,28 +5,24 @@ def personal_sum(numbers) -> ():
         try:
             result += i
         except TypeError:
+            print(f'Некорректный тип данных для подсчёта суммы - {i}')
             incorrect_data += 1
     return result, incorrect_data
 
 def calculate_average(numbers):
     try:
-        if isinstance(numbers, (int, list, tuple, set, dict)):
-            print('В numbers записан некорректный тип данных')
-        return sum(personal_sum(numbers)) / (len(numbers))
+        result, incorrect_data = personal_sum(numbers)
+    except TypeError:
+        print('В numbers записан некорректный тип данных')
+        return None
+    correct_len = len(numbers) - incorrect_data
+    try:
+        res = result / correct_len
     except ZeroDivisionError:
         return 0
-    except TypeError:
-        return None
+    return res
 
 print(f'Результат 1: {calculate_average("1, 2, 3")}')
 print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')
 print(f'Результат 3: {calculate_average(567)}')
 print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')
-
-# Результат 1: 0
-# Некорректный тип данных для подсчёта суммы - Строка
-# Некорректный тип данных для подсчёта суммы - Ещё Строка
-# Результат 2: 2.0
-# В numbers записан некорректный тип данных
-# Результат 3: None
-# Результат 4: 26.5
