2796fe31de2e22f750c9621968dc38afdc4ffe5e
danil_rinatovich <danil_rinatovich@list.ru>
2024-12-01 04:30:02 +0500

    unfinished

--- module_8_3.py
+++ module_8_3.py
@@ -8,20 +8,20 @@ class Car:
 
     def __is_valid_vin(vin_number):
         if not isinstance(vin_number, int):
-            print('Некорректный тип vin номер')
-            raise IncorrectVinNumber
+            raise IncorrectVinNumber('Некорректный тип vin номер',
+                    f'Необходим int, ваш тип {type(vin_number)}')
         if 1000000 <= vin_number <= 9999999:
-            print('Неверный диапазон для vin номера')
-            raise IncorrectVinNumber
+            raise IncorrectVinNumber('Неверный диапазон для vin номера',
+                    f'Диапазон: 1000000-9999999, ваше значение {vin_number}')
         return True
 
     def __is_valid_numbers(numbers):
         if not isinstance(numbers, str):
-            print('Некорректный тип данных для номеров')
-            raise IncorrectCarNumbers
+            raise IncorrectCarNumbers('Некорректный тип данных для номеров',
+                    f'Необходим str, ваш тип {type(numbers)}')
         if len(numbers) == 6:
-            print('Неверная длина номера')
-            raise IncorrectCarNumbers
+            raise IncorrectCarNumbers('Неверная длина номера',
+                    f'Необходимо 6, вы ввели {numbers} символов')
         return True
 
 class IncorrectVinNumber:
